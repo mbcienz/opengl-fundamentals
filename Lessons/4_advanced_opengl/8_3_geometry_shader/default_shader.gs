e2cb45f1e653ba69e18a907c45c7bbd70c35cb16
mbcienz <142720519+mbcienz@users.noreply.github.com>
2026-03-16 19:06:59 +0100

    geometry shader

---
+++ Lessons/4_advanced_opengl/8_3_geometry_shader/default_shader.gs
@@ -0,0 +1,16 @@
+#version 460 core
+layout (triangles) in;
+layout (triangle_strip, max_vertices = 3) out;
+
+
+in vec2 TexCoords[]; 
+out vec2 texCoords;
+
+void main() {    
+    for(int i = 0; i < 3; i++) {
+        gl_Position = gl_in[i].gl_Position;
+        texCoords = TexCoords[i];
+        EmitVertex();
+    }
+    EndPrimitive();
+}
---
+++ include/shaders/shader_geometry.h
@@ -0,0 +1,152 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Shader
+{
+    public: 
+        unsigned int ID; 
+
+        Shader(const char* vertexPath, const char* fragmentPath, const char* geometryPath){
+
+            /* 1. LOAD SHADERS FROM FILES */
+            std::string vertexCode;
+            std::string fragmentCode;
+            std::string geometryCode;
+            std::ifstream vShaderFile;
+            std::ifstream fShaderFile;
+            std::ifstream gShaderFile;
+            
+            //with these LoC, ifstream ca nthreow exceptions
+            vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+            fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+            gShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+            try
+            {
+                // open files
+                vShaderFile.open(vertexPath);
+                fShaderFile.open(fragmentPath);
+                gShaderFile.open(geometryPath);
+                std::stringstream vShaderStream, fShaderStream, gShaderStream;
+                
+                //read file buffer 
+                vShaderStream << vShaderFile.rdbuf();
+                fShaderStream << fShaderFile.rdbuf();
+                gShaderStream << gShaderFile.rdbuf();
+                //close file handlers
+                vShaderFile.close();
+                fShaderFile.close();
+                gShaderFile.close();
+
+                //convert steram into string
+                vertexCode = vShaderStream.str();
+                fragmentCode = fShaderStream.str();
+                geometryCode = gShaderStream.str();
+            }
+            catch(std::ifstream::failure e)
+            {
+                std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+            }
+
+            const char* vShaderCode = vertexCode.c_str();
+            const char* fShaderCode = fragmentCode.c_str();
+            const char* gShaderCode = geometryCode.c_str();
+
+            /* 2. COMPILE SHADERS */
+            unsigned int vertex, fragment, geometry;
+            int success; 
+            char infoLog[512];
+
+            //vertex shader
+            vertex = glCreateShader(GL_VERTEX_SHADER);
+            glShaderSource(vertex, 1, &vShaderCode, NULL);
+            glCompileShader(vertex);
+            glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+            if(!success){
+                glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+                std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+            }
+
+            //fragment shader
+            fragment = glCreateShader(GL_FRAGMENT_SHADER);
+            glShaderSource(fragment, 1, &fShaderCode, NULL);
+            glCompileShader(fragment);
+            glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+            if(!success){
+                glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+                std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+            }
+
+            //geometry shader
+            geometry = glCreateShader(GL_GEOMETRY_SHADER);
+            glShaderSource(geometry, 1, &gShaderCode, NULL);
+            glCompileShader(geometry);
+            glGetShaderiv(geometry, GL_COMPILE_STATUS, &success);
+            if(!success){
+                glGetShaderInfoLog(geometry, 512, NULL, infoLog);
+                std::cout << "ERROR::SHADER::GEOMETRY::COMPILATION_FAILED\n" << infoLog << std::endl;
+            }
+
+            /* 3. create Shader program */
+            ID = glCreateProgram();
+            glAttachShader(ID, vertex);
+            glAttachShader(ID, fragment);
+            glAttachShader(ID, geometry);
+            glLinkProgram(ID);
+            glGetProgramiv(ID, GL_LINK_STATUS, &success);
+            if(!success){
+                glGetProgramInfoLog(ID, 512, NULL, infoLog);
+                std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+            }
+
+            // delete shader
+            glDeleteShader(vertex);
+            glDeleteShader(fragment);
+            glDeleteShader(geometry);
+        };
+
+        void use()
+        {
+            glUseProgram(ID);
+        };
+
+        void setBool(const std::string &name, bool value) const
+        {
+            glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+        };
+        void setInt(const std::string &name, int value) const
+        {
+            glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+        };
+
+        void setFloat(const std::string &name, float value) const
+        {
+            glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+        };
+
+        void setMat4(const std::string &name, glm::mat4 mat) const
+        {
+            glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
+        }
+
+        void setVec3(const std::string &name, float x, float y, float z) const
+        {
+            glUniform3f(glGetUniformLocation(ID, name.c_str()), x , y, z);
+        }
+
+        void setVec3(const std::string &name, glm::vec3 vec) const
+        {
+            glUniform3f(glGetUniformLocation(ID, name.c_str()), vec.x , vec.y, vec.z);
+        }
+};
+
+#endif
